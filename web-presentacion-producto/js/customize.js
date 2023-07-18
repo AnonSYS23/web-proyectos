8c8ad693d85e18e43cf7512517bb0a07cedc9725
AnonDEV23 <luisayvalballesteros@icloud.com>
2023-07-18 18:35:52 -0500

    LOL

--- web-presentacion-producto/js/customize.js
+++ web-presentacion-producto/js/customize.js
@@ -1,19 +1,12 @@
-// Código JavaScript para el segundo código HTML
-
-// Función para mostrar una alerta cuando se hace clic en el botón "Submit"
+// Función para procesar el formulario al hacer clic en el botón "Submit"
 function submitCustomization() {
-    alert('Your customization has been submitted!');
-  }
+  const colorSelect = document.getElementById('color');
+  const selectedColor = colorSelect.value;
+
+  // Agregar una clase al formulario basado en el color seleccionado
+  const form = document.getElementById('customization-form');
+  form.classList.remove('form-color-black', 'form-color-silver', 'form-color-blue');
+  form.classList.add(`form-color-${selectedColor}`);
   
-  // Obtener el formulario de customización por su ID
-  var customizationForm = document.getElementById('customization-form');
-  
-  // Agregar un evento de clic al botón del formulario de customización
-  customizationForm.addEventListener('submit', function(event) {
-    // Detener el envío del formulario
-    event.preventDefault();
-    
-    // Llamar a la función submitCustomization cuando se hace clic en el botón
-    submitCustomization();
-  });
-  
+  // Aquí puedes agregar más lógica para procesar los datos del formulario si es necesario
+}
